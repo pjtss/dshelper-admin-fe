102bf1f0873d47dbcac17d2a76372a47f904c6f2
lee <dldbsgh00005@gmail.com>
2026-04-19 17:24:22 +0900

    Fix Kakao login endpoint

--- dshelperAdmin/src/utils/kakaoAuth.js
+++ dshelperAdmin/src/utils/kakaoAuth.js
@@ -1,20 +1,14 @@
 ﻿export const KAKAO_CLIENT_ID = "f6e09eeb053caaad27d60a4e451fd9bc";
 export const KAKAO_REDIRECT_URI = "https://admin.dshelper.kr/oauth/kakao/callback";
-export const KAKAO_AUTHORIZE_URL = "https://kauth.kakao.com/oauth/authorize";
-export const KAKAO_TOKEN_EXCHANGE_URL = "https://server.dshelper.kr/oauth/kakao/logn";
+export const KAKAO_LOGIN_URL = "https://server.dshelper.kr/oauth/kakao/login";
+export const KAKAO_TOKEN_EXCHANGE_URL = "https://server.dshelper.kr/oauth/kakao/login";
 
 export function getKakaoRedirectUri() {
   return KAKAO_REDIRECT_URI;
 }
 
 export function createKakaoLoginUrl() {
-  const params = new URLSearchParams({
-    client_id: KAKAO_CLIENT_ID,
-    redirect_uri: KAKAO_REDIRECT_URI,
-    response_type: "code",
-  });
-
-  return `${KAKAO_AUTHORIZE_URL}?${params.toString()}`;
+  return KAKAO_LOGIN_URL;
 }
 
 export function extractKakaoCode(search) {
